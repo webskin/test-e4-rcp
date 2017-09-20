3583f8e4179e1ed2ca56de18a2adf335ea4d5df6
mickael <mickael.gauvin.external@atos.net>
2017-09-20 17:22:24 +0200

    simple project

--- com.example.e4.rcp/src/testing/handlers/SaveHandler.java
+++ com.example.e4.rcp/src/testing/handlers/SaveHandler.java
@@ -2,12 +2,13 @@ package testing.handlers;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
 public class SaveHandler {
 
 	@CanExecute
-	public boolean canExecute(EPartService partService) {
+	public boolean canExecute(EPartService partService, IWorkbench workbench) {
 		if (partService != null) {
 			return !partService.getDirtyParts().isEmpty();
 		}
